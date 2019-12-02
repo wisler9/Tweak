e81d068c5eafac55d61a0d246caa9e57720d391d
anvcodes <kpeace0720@gmail.com>
2019-12-02 14:47:55 -0500

    models

--- server/server.js
+++ server/server.js
@@ -1,1 +1,12 @@
-require Express
+var db = require("./models");
+
+db.sequelize.sync(syncOptions).then(function() {
+    app.listen(PORT, function() {
+      console.log(
+        "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+        PORT,
+        PORT
+      );
+    });
+  });
+  
