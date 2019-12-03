4deec0a932fcb9d7efccf6d21782b95f131d8a80
Logan <wislermichel@gmail.com>
2019-12-03 14:55:08 -0500

    Login and Signup created

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SignUp from "./components/SignUp";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <div>
+      <Login />
+      <SignUp />
+    </div>
+  );
+}
+
+export default App;
