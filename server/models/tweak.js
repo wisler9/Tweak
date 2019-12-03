3d8119bab4a2906f65da2c2b63a74f8626377ed3
anvcodes <kpeace0720@gmail.com>
2019-12-03 12:37:45 -0500

    remove gitignore files

--- server/models/tweak.js
+++ server/models/tweak.js
@@ -10,7 +10,7 @@ module.exports = function(sequelize, DataTypes) {
             allowNull: false,
             defaultValue: 2
           },
-          as: "tweaks",
+          as: "userTweaks",
     
           through: models.User
         });
