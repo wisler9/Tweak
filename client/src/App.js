9e4423d8fa993c34f5795c2af48179ab21c6e940
Logan <wislermichel@gmail.com>
2019-12-05 15:29:52 -0500

    worked on login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
-import SignUp from "./components/signUp-form";
+// import SignUp from "./components/signUp-form";
 import LoginForm from "./components/login-form";
+// import Login from "./components/Login";
 
 function App() {
   return (
     <div>
+      {/* <Login /> */}
       <LoginForm />
-      <SignUp />
+      {/* <SignUp /> */}
     </div>
   );
 }
