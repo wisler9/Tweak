96d3b39bbadb4a64c017cf5a42c480b6c6b96bbb
Logan <wislermichel@gmail.com>
2019-12-05 15:20:39 -0500

    authentication

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import {gql, graphql} from "react-apollo";
-import LoginForm from "./login-form";
-import SignUp from "signUp-form";
+// import LoginForm from "./login-form";
+// import SignUp from "signUp-form";
 
 class Login extends React.Component {
     state = {
@@ -15,19 +14,30 @@ class Login extends React.Component {
         });
     }
 
-    onSubmit = async () => {
-        const response = await this.props.mutate({
-            variables: this.state,
-        });
-        const { token, refreshToken } = response.data.login;
-        console.log(response);
-        localStorage.setItem("token", token);
-        localStorage.setItem("refreshToken", refreshToken);
-    }
+    // onSubmit =  (e) => {
+    //     e.preventDefault()
+    //     console.log("I am cool")
+    // onSubmit = async () => {
+        // const response = await this.props.mutate({
+        //     variables: this.state,
+        // });
+        // const { token, refreshToken } = response.data.login;
+        // console.log(response);
+        // localStorage.setItem("token", token);
+        // localStorage.setItem("refreshToken", refreshToken);
+    // }
+
+    handleSubmit(event) {
+        alert('An essay was submitted: ' + this.state.value);
+        console.log("I am cool")
+        event.preventDefault();
+      }
+
 
     render(){
         return (
             <div>
+            <form onSubmit={this.handleSubmit}>
                 <input
                     name="email"
                     placeholder="Email"
@@ -40,19 +50,28 @@ class Login extends React.Component {
                     onChange={e=> this.onChange(e)}
                     value={this.state.password} /> 
                 <br />
-                <button onClick={() => this.onSubmit()} type="primary">Login</button>   
+                
+                    {/* <label>
+                    Essay:
+                    <textarea value={this.state.value} onChange={this.handleChange} />
+                    </label> */}
+                    <input type="submit" value="Submit" />
+                </form>
+
+                {/* <button onSubmit={this.handleSubmit} type="primary">Login</button>    */}
+                {/* <button onClick={this.onSubmit} type="primary">Login</button>    */}
             </div>
         );
     }
 }
 
-const mutation = gql`
-mutation ($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-        token
-        refresToken
-    }
-}
-`;
+// const mutation = gql`
+// mutation ($email: String!, $password: String!) {
+//     login(email: $email, password: $password) {
+//         token
+//         refresToken
+//     }
+// }
+// `;
 
-export default graphql(mutation)(Login);
+export default Login;
--- client/src/components/login-form.jsx
+++ client/src/components/login-form.jsx
@@ -1,32 +1,58 @@
 import React from 'react';
-import CreateUser from "./CreateUser"
+
+const users = []
+if(user) {
+    this.state ===
+}
 
 
-function LoginForm() {
-    return(
-     <div className="container">
-        <form>
-            <div className="form-group">
-                <label for="inputEmail">Email address</label>
-                <input type="email" className="form-control" id="inputEmail" aria-describedby="emailHelp" placeholder="Enter email" />
-                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-            </div>
-            <div class="form-group">
-                <label for="inputPassword">Password</label>
-                <input type="password" className="form-control" id="inputPassword" placeholder="Password" />
-            </div>
-            <div className="form-group form-check">
-                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                <label className="form-check-label" for="exampleCheck1">Check me out</label>
-            </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
-    </div>
-  );
-}   
+
+class Login extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {value: ''};
+  
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+  
+    handleSubmit(event) {
+      alert('A name was submitted: ' + this.state.value);
+      event.preventDefault();
+    }
+
+
+    render() {
+        return(
+        <div className="container">
+            <form onSubmit={this.handleSubmit}>
+                <div className="form-group">
+                    <label for="inputEmail">Email address</label>
+                    <input type="email" className="form-control" id="inputEmail" aria-describedby="emailHelp" placeholder="Enter email" onChange={this.handleChange} />
+                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+                <div class="form-group">
+                    <label for="inputPassword">Password</label>
+                    <input type="password" className="form-control" id="inputPassword" placeholder="Password" onChange={this.handleChange} />
+                </div>
+                <div className="form-group form-check">
+                    <input type="checkbox" className="form-check-input" id="exampleCheck1" onChange={this.handleChange}/>
+                    <label className="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    );
+    } 
+}  
+    
 
 
        
 
 
-export default LoginForm;
+export default Login;
