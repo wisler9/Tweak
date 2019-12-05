918a9e8871f39dfe38bf7f93e0580a27113605f6
Romy Owens <rdoed160@gmail.com>
2019-12-05 10:29:54 -0500

    creating files for components and pages

--- client/src/components/ListItems/index.js
+++ client/src/components/ListItems/index.js
@@ -1,18 +1,18 @@
 import React,  {Component} from 'react';
 
 const ListItems= (props) => {
-        console.log(props)
-        return (
-            <div>
-                <img src="..." className="card-img" alt="..."/>
-                {/* <h5 className="card-title">Tweak Title</h5> */}
-                    {props.tweaks.map(tweak => (
-                    <div className="card">
-                    {tweak}
-                    </div>
-                    ))}
-            </div>
-        );
+    console.log(props)
+    return (
+        <div>
+            <img src="..." className="card-img" alt="..."/>
+            {/* <h5 className="card-title">Tweak Title</h5> */}
+                {props.tweaks.map(tweak => (
+                <div className="card">
+                {tweak}
+                </div>
+                ))}
+        </div>
+    );
 };
 
 export default ListItems;
