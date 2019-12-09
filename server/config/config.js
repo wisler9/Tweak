5db23a58ac8b355f28e7f7bcb14c9df3fb8342e0
Romy Owens <rdoed160@gmail.com>
2019-12-09 10:39:58 -0500

    converted config.json to config.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
-{
+module.exports = {
     "development": {
       "username": "root",
-      "password": "Rasengan@9",
+      "password": process.env.PASSWORD,
       "database": "tweak_DB",
       "host": "localhost",
       "dialect": "mysql"
@@ -19,3 +19,4 @@
       "dialect": "mysql"
     }
   }
+
