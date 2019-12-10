283bd4e13c9521a3276f45f6bf08b9a4599dd91e
Logan <wislermichel@gmail.com>
2019-12-09 20:42:41 -0500

    updated the signup page

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,38 +1,81 @@
 import React from "react";
 import "../components/SignUp/style.css";
+import axios from  "axios";
 
-function SignUp() {
-  return (
-    <div className="container signup">
-        <div className="row head">Create your account</div>
-        <h1 className="logo">&lt;TWEAK/&gt;</h1>
-        <a href="https://placeholder.com"/><img src="https://via.placeholder.com/200" alt="" className="img-thumbnail" placeholder="Image"></img>
-        <form>
-            <div className="form-group signup">
-                <label for="userName">User Name</label>
-                <input type="text" className="form-control" id="exampleUserName1" aria-describedby="userName" placeholder="Enter username" />
+
+class SignUp extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        username:"",
+        email:"",
+        password:"",
+        bio:"",
+        experience:""
+      };
+  
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleChange(event) {
+        // event.preventDefault();
+        this.setState({[event.target.name] : event.target.value});
+        //   console.log(event.target.name);
+    }
+  
+    handleSubmit(event) {
+        // console.log(this.state.userName)
+        console.log("dssddfs")
+      event.preventDefault();
+      axios.post('/api/user', {
+        userName: this.state.userName,
+        email: this.state.email,
+        password: this.state.password,
+        bio: this.state.bio,
+        experience: this.state.experience,
+      }).then( res => {
+            console.log(res)
+      })
+    }
+
+    
+
+
+    render() {
+            console.log(this.state);
+            return (
+            <div className="container signup">
+                {/* <div className="row head">Create your account</div>
+                <h1 className="logo">&lt;TWEAK/&gt;</h1>
+                <a href="https://placeholder.com"/><img src="https://via.placeholder.com/200" alt="" className="img-thumbnail" placeholder="Image"></img> */}
+                <form>
+                    <div className="form-group signup">
+                        <label for="userName">User Name</label>
+                        <input type="text" onChange={this.handleChange} name="username"className="form-control" id="userName" aria-describedby="userName" placeholder="Enter username" />
+                    </div>
+                    <div className="form-group signup">
+                        <label for="userEmail">Email address</label>
+                        <input type="email" onChange={this.handleChange} name="email"  className="form-control" id="userEmail" aria-describedby="emailHelp" placeholder="Enter email" />
+                    </div>
+                    <div className="form-group signup">
+                        <label for="userPassword">Password</label>
+                        <input type="password" onChange={this.handleChange} name="password" className="form-control" id="userPassword" placeholder="Password" />
+                    </div>
+                    <div className="form-group signup">
+                        <label for="bio">userBio</label>
+                        <textarea onChange={this.handleChange}  name="bio" className="form-control" id="userBio" rows="3"></textarea>
+                    </div>
+                    
+                    <div className="form-group signup">
+                        <label for="userExperience">Experience</label>
+                        <textarea onChange={this.handleChange}  name="experience" className="form-control" id="userExperience" rows="3"></textarea>
+                    </div>
+                    <button type="submit" className="btn btn-secondary createBtn">Submit</button>
+                    </form>
             </div>
-            <div className="form-group signup">
-                <label for="InputEmail1">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-            </div>
-            <div className="form-group signup">
-                <label for="InputPassword1">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
-            </div>
-            <div className="form-group signup">
-                <label for="bio">Bio</label>
-                <textarea className="form-control" id="bioTextarea" rows="3"></textarea>
-            </div>
-            
-            <div className="form-group signup">
-                <label for="experience">Experience</label>
-                <textarea className="form-control" id="experienceTextarea" rows="3"></textarea>
-            </div>
-            <button type="submit" className="btn btn-secondary createBtn">Submit</button>
-            </form>
-    </div>
-  );
+        );
+    }
 }
 
 
